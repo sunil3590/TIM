e988e3104c2823ce02682625c3a8781c3d4d05e5
Sunil D S <sunil3590@gmail.com>
2013-08-26 18:57:56 -0400

    import error in stop_all.py

--- wolfbot/motion/stop_all.py
+++ wolfbot/motion/stop_all.py
@@ -3,8 +3,6 @@ sys.path.append('/wolfbot/agent')
 import wolfbot as wb
 from time import time
 from time import sleep
-sys.path.append('../sensors/')
-from ir_ain import IR_AIN
 
 w = wb.wolfbot()
 w.move(0,0)
