247fea883512782ab34040c8fa10ea41fb05ed58
ngohara <ngohara@ncsu.edu>
2016-03-22 21:36:51 -0400

    updating sensor code and example scripts

---
+++ wolfbot_code/sensors/test_color.py
@@ -0,0 +1,25 @@
+import cs_class
+from time import time
+
+cs = cs_class.color_senser(1)
+
+if cs.valid_init:
+	print "Valid color sensor"
+else :
+	print "Color Sensor invalid"
+
+cs.setUpperThreshold(0x5)
+
+print "UT : " + str( cs.readUpperThreshold())
+
+
+for x in range(5):
+	t0 = time()
+	print "\nStatus: " + cs.readStatus()
+	print "Red  : " + str( cs.readRed())
+	print "Green: " + str( cs.readGreen())
+	print "Blue : " + str( cs.readBlue())
+	t1 = time()
+	print "Sample time: " + str( int((t1-t0)*10000)/10.0) + "ms"
+
+
