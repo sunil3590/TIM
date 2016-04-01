5530ce376345358cd547dc58cca414aed712646d
Sunil D S <sunil3590@gmail.com>
2016-03-31 22:49:49 -0400

    code clean up and architecture doc

--- wolfbot/bot.py
+++ wolfbot/bot.py
@@ -1,27 +1,30 @@
+#!/usr/bin/python
+
+import sys
 import paho.mqtt.client as mqtt
 
-# The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, rc):
-    print("Connected with result code " + str(rc))
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("TIM/CAR/C_ID/COMMAND")
-
 # The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
-    print("Topic: ", msg.topic, "Message: ", str(msg.payload))
+def on_command(client, userdata, msg):
+	print msg.payload
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
+# process command line arguments
+if len(sys.argv) != 2:
+	print("Usage : python tim.py <BOT_ID>")
+bot_id = str(sys.argv[1])
 
-client.connect("localhost", 1883, 60)
+# TOPIC to subscribe to
+topic = "tim/wolfbot/" + bot_id + "/command"
+print topic
+
+# initialze a client and connect to the server
+client = mqtt.Client(client_id="bot_" + bot_id)
+client.on_message = on_command
+client.connect(host="localhost", port=1883, keepalive=60)
+client.subscribe(topic)
 
 # request TIM to cross
-mqttc = mqtt.Client("python_pub")
-mqttc.connect("localhost", 1883)
-mqttc.publish("TIM/J_ID/REQUEST", "{speed:35,enter:yellow,exit:blue}")
-mqttc.loop(2)
+client.publish("tim/27606/request", "{speed:35,enter:yellow,exit:blue}")
+client.loop(2)
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
