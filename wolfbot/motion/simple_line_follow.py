164bcc7624b0e06cb0bfef3536dd72ad3da2efb2
ngohara <ngohara@ncsu.edu>
2016-04-03 19:46:20 -0400

    changes to make simple work, not good algorithm though

--- wolfbot/motion/simple_line_follow.py
+++ wolfbot/motion/simple_line_follow.py
@@ -3,9 +3,14 @@ sys.path.append('/wolfbot/agent')
 import wolfbot as wb
 from time import time
 from time import sleep
+sys.path.append('../sensors/')
 from ir_ain import IR_AIN
 
 w = wb.wolfbot()
+w.move(0,0)
+sleep(5)
+
+
 ir = IR_AIN()
 
 print "batt voltage: " + str(w.battery.voltage() ) + "\n"
@@ -13,26 +18,32 @@ print "batt voltage: " + str(w.battery.voltage() ) + "\n"
 current_dir = 'left'
 
 def switch_dir(old, speed):
-	if old == 'left':
-		w.move( 10, speed)
-		return 'right'
-	else :
-		w.move( 350, speed)
-		return 'left'
+        if old == 'left':
+                w.move( 3, speed)
+                return 'right'
+        else :
+                w.move( 357, speed)
+                return 'left'
 
 
-drive_speed = 45
+drive_speed = 50
 
-t_end = time() + 60*2	# 2 mins
+t_end = time() + 60*2   # 2 mins
 current_dir = switch_dir( current_dir, drive_speed) #start moving
-#while time() < t_end:
-for x in range(20)	#approx 20s
-	if ir.travel_is_white() :
-		current_dir = switch_dir(current_dir, drive_speed)
-		print current_dir
+black_check = 0         #seen black yet? don't change dir
 
-	sleep(1)
+#while time() < t_end:
+for x in range(15):      #approx 20s
+        print "Is White: " + str(ir.travel_is_white())
+
+        if ir.travel_is_white() and black_check :
+                current_dir = switch_dir(current_dir, drive_speed)
+                print "Dir : " + current_dir
+                black_check = 0
+        else:
+                black_check = 1
+
+        sleep(0.1)
 
 w.move(0,0)
-
 
