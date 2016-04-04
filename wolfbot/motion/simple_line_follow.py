c1fc38fa85c74972c2be654ae7c8a7347a57906b
ngohara <ngohara@ncsu.edu>
2016-04-04 00:28:37 -0400

    made simple work with time_rotate to find line

--- wolfbot/motion/simple_line_follow.py
+++ wolfbot/motion/simple_line_follow.py
@@ -12,6 +12,8 @@ sleep(5)
 
 
 ir = IR_AIN()
+ir.set_thresh(0.5)
+
 
 print "batt voltage: " + str(w.battery.voltage() ) + "\n"
 
@@ -26,6 +28,30 @@ def switch_dir(old, speed):
                 return 'left'
 
 
+# use rotating movements to find line
+def rot_line():
+        dr = 1
+        theta_l = [-25, 23]     #bot 14 drifts left, need more power on right swing
+        tw = 1
+        td = 0.02
+        tot = 0
+        while(1):
+                t0 = time()
+                w.rotate( theta_l[dr] )
+                while ir.travel_val() < ir.thresh:
+                        print ir.travel_val()
+                        if (time()-t0) > (tw*td) :
+                                break
+                w.rotate(0)
+                tot += tw*td
+                tw += 1
+                dr = (dr+1)%2
+                if ir.travel_val() >= ir.thresh:
+                        print tw
+                        print tot
+                        return
+
+
 drive_speed = 50
 
 t_end = time() + 60*2   # 2 mins
@@ -33,17 +59,13 @@ current_dir = switch_dir( current_dir, drive_speed) #start moving
 black_check = 0		#seen black yet? don't change dir
 
 #while time() < t_end:
-for x in range(15):      #approx 20s
-        print "Is White: " + str(ir.travel_is_white())
-
-	if ir.travel_is_white() and black_check :
-                current_dir = switch_dir(current_dir, drive_speed)
-                print "Dir : " + current_dir
-		black_check = 0
+for x in range(20):      #approx 20s
+	if ir.travel_val() >= ir.thresh:       
+		w.move(0, drive_speed)
 	else:
-		black_check = 1
+		rot_line()
 
-        sleep(0.1)
+	sleep(0.2)
 
 w.move(0,0)
 
